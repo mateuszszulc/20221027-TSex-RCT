4fc8eae15bd57fa2b4bff86305f2ccf0e305bb9e
Jabi <michal.jabi@gmail.com>
2022-10-28 11:23:37 +0200

    migration(52): step 1: config + first .ts file

--- 5-migrate-to-ts/52-vanilla-project/src/main.ts
+++ 5-migrate-to-ts/52-vanilla-project/src/main.ts
@@ -2,4 +2,8 @@ import { root } from './shop/root.component.js'
 
 const $app = document.querySelector('#app')
 
-$app.append(root())
+if ($app) {
+    $app.append(root())
+}
+
+console.log('Hello ?')
