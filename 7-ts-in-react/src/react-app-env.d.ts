c7a6569f5820eeb87f05e1ebc92cbbfb1a5b6145
Jabi <michal.jabi@gmail.com>
2022-10-28 14:27:59 +0200

    explain(7): .d.ts in .env  - extend types for environment variables

--- 7-ts-in-react/src/react-app-env.d.ts
+++ 7-ts-in-react/src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        // podpowiadaj mi składnie do:
+        // process.env.REACT_APP_TITLE - że to jest i że to jest string !
+        readonly REACT_APP_TITLE: string
+    }
+}
