7610b7fcf6ec3409feb689ab0014658a53c645af
Jabi <michal.jabi@gmail.com>
2022-10-28 16:22:06 +0200

    migrate-react(6): step 3 - fix types .tsx for initial files

--- 6-power-your-project-with-typescript/src/index.tsx
+++ 6-power-your-project-with-typescript/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 
-import { BrowserRouter } from 'react-router-dom'
 import { store } from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
